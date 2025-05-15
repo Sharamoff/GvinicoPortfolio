2f5a5317c620612e3429cf4d84bbd770fa6b2519
Ihar Sharamau <sharamoff@gmail.com>
2025-05-15 10:19:04 +0200

    Map section + page animation

--- SRC/js/lottie-animation.js
+++ SRC/js/lottie-animation.js
@@ -1,18 +1,23 @@
 import {DotLottie} from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
 
-
 const animations = {
-	'logo-lottie': new DotLottie({
+	'hdr-logo-lottie': new DotLottie({
 		autoplay: true,
 		loop: true,
-		canvas: document.querySelector('#logo-lottie'),
+		canvas: document.querySelector('#hdr-logo-lottie'),
 		src: "animation/hdr-logo.lottie"
 	}),
-	'illustration-lottie': new DotLottie({
+	'hdr-illustration-lottie': new DotLottie({
 		autoplay: true,
 		loop: true,
-		canvas: document.querySelector('#illustration-lottie'),
+		canvas: document.querySelector('#hdr-illustration-lottie'),
 		src: "animation/hdr-illustration.lottie"
+	}),
+	'hdr-map-lottie': new DotLottie({
+		autoplay: true,
+		loop: true,
+		canvas: document.querySelector('#hdr-map-lottie'),
+		src: "animation/hdr-logo.lottie"
 	})
 };
 
