2f6b0ada1933658a03e86b0bc34221c561c90be5
Ihar Sharamau <sharamoff@gmail.com>
2025-06-02 17:28:30 +0200

    Illustration section GSAP animation

--- BUILD/js/lottie-animation.js
+++ BUILD/js/lottie-animation.js
@@ -1,4 +1,3 @@
-// lottie-animation.js
 import {DotLottie} from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
 
 const animations = {
@@ -21,43 +20,3 @@ const animations = {
 		src: "animation/hdr-map.lottie"
 	})
 };
-
-const timeouts = {};
-
-const restartAnimation = (animationId) => {
-	const animation = animations[animationId];
-	clearTimeout(timeouts[animationId]);
-	timeouts[animationId] = setTimeout(() => {
-		animation.stop();
-		animation.setFrame(0);
-		animation.play();
-	}, 100);
-};
-
-if (window.innerWidth > 576 && window.innerHeight > 576) {
-	const observer = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			const section = entry.target;
-			const canvas = section.querySelector('canvas');
-			if (!canvas) return;
-			const animationId = canvas.id;
-			if (entry.isIntersecting) {
-				restartAnimation(animationId);
-			}
-		});
-	}, {
-		root: null,
-		rootMargin: '0px',
-		threshold: 0.1
-	});
-	
-	const sections = document.querySelectorAll('.content');
-	sections.forEach(section => observer.observe(section));
-	
-	Object.keys(animations).forEach(animationId => {
-		const canvas = document.querySelector(`#${animationId}`);
-		canvas.addEventListener('click', () => {
-			restartAnimation(animationId);
-		});
-	});
-}
--- SRC/js/gsap-animation.js
+++ SRC/js/gsap-animation.js
@@ -1,15 +1,22 @@
-// gsap-animation.js
-
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
 
   gsap.set([
+    
     ".gvlogo__tl .taskdesc",
     ".gvlogo__tr .taskdesc",
     ".gvlogo__bl img",
     ".gvlogo__br img",
-    ".gvlogo-biglogo img"
+    ".gvlogo-biglogo img",
+    
+    ".gvillustration__l .taskdesc",
+    ".gvillustration__l .g1",
+    ".gvillustration__l .g2-l",
+    ".gvillustration__l .g2-r",
+    ".gvillustration__l .g4-l",
+    ".gvillustration__r .taskdesc",
+    
   ], {
     opacity: 0
   });
@@ -19,6 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.set(".gvlogo__bl img", { x: -150 });
   gsap.set(".gvlogo__br img", { x: 150 });
   gsap.set(".gvlogo-biglogo img", { scale: 0.5 });
+  
+  gsap.set(".gvillustration__l .taskdesc", { x: -150 });
+  gsap.set(".gvillustration__l .g1", { y: 50 });
+  gsap.set(".gvillustration__l .g2-l", { y: 50 });
+  gsap.set(".gvillustration__l .g2-r", { y: 50 });
+  gsap.set(".gvillustration__l .g4-l", { y: 50 });
+  gsap.set(".gvillustration__r .taskdesc", { x: 150 });
+  gsap.set(".gvillustration__r .g4-r", { y: 600 });
+  gsap.set(".gvillustration__r .g3-l", { y: 600 });
+  gsap.set(".gvillustration__r .g3-r", { y: 600 });
+  gsap.set(".gvillustration__r .g0", { y: 600 });
+  gsap.set(".gvillustration__r .blockill-stains", { x: 1000 });
+  
 
   // Logo
   
@@ -26,11 +46,11 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollTrigger: {
       trigger: "#gvlogo",
       start: "top top",
-      end: "+=150%",
+      end: "+=100%",
       scrub: 1,
       pin: true,
       pinSpacing: true,
-      toggleActions: "play none none reverse"
+      toggleActions: "play none none reverse",
     }
   });
 
@@ -74,38 +94,103 @@ document.addEventListener("DOMContentLoaded", () => {
   
   
   
+  // Illustration
   
-  
-  
-  // scroll Icon
-  
-  gsap.set(".i-scroll", {
-    yPercent: -50,
-    xPercent: -50,
-    left: "50%",
-    top: "50%",
-    opacity: 1
+  const TL_Illustration = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#gvillustration",
+      start: "top top",
+      end: "+=100%",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+      toggleActions: "play none none reverse"
+    }
   });
+
+  TL_Illustration.to(".gvillustration__l .taskdesc", {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  });
+
+  TL_Illustration.to(".gvillustration__l .g1", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=1.0");
+
+  TL_Illustration.to(".gvillustration__l .g2-l", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__l .g2-r", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__l .g4-l", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__r .taskdesc", {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__r .g4-r", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=1.0");
+
+  TL_Illustration.to(".gvillustration__r .g3-l", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__r .g3-r", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.5");
+
+  TL_Illustration.to(".gvillustration__r .g0", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out"
+  }, "-=0.1");
   
-  const scrollTimeline = gsap.timeline();
+  TL_Illustration.to(".gvillustration__r .blockill-stains", {
+    x: 0,
+    opacity: 1,
+    duration: 3,
+    ease: "power2.out"
+  }, "-=0.5");
   
-  scrollTimeline
-    .to(".i-scroll", {
-      top: "90%",
-      delay: 3,
-      duration: 1,
-      ease: "power2.inOut"
-    })
-    .to(".i-scroll", {
-      opacity: 0,
-      duration: 3,
-      delay: 6,
-      ease: "power1.inOut"
-    });
-  
-  // end scroll Icon
+  // end Illustration
 
 
+
+  
+  
   
 });
 
